228bbff547924c0c5451d129bc80c40abeef16be
Ernest Awuku Junior <awukuernest@gmail.com>
2025-10-19 22:01:35 +0000

    changes

---
+++ task_2.sql
@@ -0,0 +1,40 @@
+USE `alx_book_store`;
+
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS `Books` (
+    `book_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(130) NOT NULL,
+    `author_id` INT UNSIGNED NOT NULL,
+    `price` DOUBLE,
+    `publication_date` DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE CASCADE ON UPDATE CASCADE,
+);
+
+CREATE TABLE IF NOT EXISTS `Customers` (
+    customer_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `Orders` (
+    order_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    customer_id INT UNSIGNED NOT NULL,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE ON UPDATE CASCADE,
+);
+
+CREATE TABLE IF NOT EXISTS `Order_Details` (
+    orderdetailid INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id INT UNSIGNED NOT NULL,
+    book_id INT UNSIGNED NOT NULL,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE ON UPDATE CASCADE,
+);
